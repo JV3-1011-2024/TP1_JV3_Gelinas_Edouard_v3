ccb7ee0f9506dfead6ff87a3373221021623bab1
Édouard Gélinas <edougel29@gmail.com>
2024-10-19 14:52:13 -0400

    porte anim bugz

--- Assets/Scripts/Serrure.cs
+++ Assets/Scripts/Serrure.cs
@@ -5,6 +5,7 @@ public class Serrure : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip sonCleDansSerrure;
     private bool cleInseree = false;
+    public Animator porteAnimator;
 
     void Start()
     {
@@ -26,5 +27,6 @@ public class Serrure : MonoBehaviour
     {
         audioSource.PlayOneShot(sonCleDansSerrure);
         cleInseree = true;
+        porteAnimator.SetBool("isOpened", true);
     }
 }
